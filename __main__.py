ff03bffcfd8f54e77812d081d43669e1e899c6b4
chrstnbwnkl <c.beiwinkel@students.uu.nl>
2021-02-05 11:07:54 +0100

    v1.0.0

--- __main__.py
+++ __main__.py
@@ -6,10 +6,14 @@ from dotenv import dotenv_values
 
 def main():
     env = dotenv_values()
-    conn = db.connect(**{key: env[key] for key in ["dbname", "username", "passwort", "host", "port"]})
-    iter_conn = db.connect(**{key: env[key] for key in ["dbname", "username", "passwort", "host", "port"]})
-    gc = Geocoder(conn, iter_conn)
+    db_creds= {key: env[key] for key in ["dbname", "user", "password", "host", "port"]}
+    conn = db.connect(**db_creds)
+    iter_conn = db.connect(**db_creds)
+    gc = Geocoder(conn, iter_conn, debug=False)
 
+    gc.geocode()
+    conn.close()
+    iter_conn.close()
 
 if __name__ == "__main__":
     main()
--- geocoder.py
+++ geocoder.py
@@ -1,4 +1,6 @@
 import json
+from requests import get, post
+from dotenv import dotenv_values
 
 class Geocoder:
     """Geocoder class that provides batch geocoding functionalities for the ArcGIS world geocoding REST API.
@@ -17,6 +19,7 @@ class Geocoder:
         if iter_conn:
             self._iter_cur = iter_conn.cursor()
         self._debug = debug
+        self._env = dotenv_values()
     
     @property
     def conn(self):
@@ -43,7 +46,7 @@ class Geocoder:
         WHERE location->'location'->'additionalNames'->>'long'::varchar IS NOT null
         """
         if self._debug == True:
-            querystring += " LIMIT 5"
+            querystring += " LIMIT 230"
         
         query = self._iter_cur.mogrify(querystring)
         return self._iter_cur.execute(query)
@@ -65,7 +68,7 @@ class Geocoder:
             if row == None:
                 break
 
-            if self._exists(row):
+            if not self._exists(row):
                 filtered_locations.append(row)
             
         return filtered_locations
@@ -74,7 +77,7 @@ class Geocoder:
 
     def _exists(self, row):
         querystring_template = f"""
-        SELECT exists (SELECT 1 FROM user_locations WHERE location->>'locationId'::varchar = {row[1]} LIMIT 1);
+        SELECT exists (SELECT 1 FROM user_locations WHERE id = {row[1]} LIMIT 1);
         """
         query = self._cur.mogrify(querystring_template)
         self._cur.execute(query)
@@ -129,7 +132,61 @@ class Geocoder:
         for i in range(0, len(lst), n):
             yield lst[i:i + n]
 
-    def geocode(self, locations):
+    def _get_token(self, user, secret):
+        """Generate an ArcGIS token with your ArcGIS user credentials
+        
+        Parameters
+        ----------
+        user : str
+            the user key generated on your ArcGIS dev portal page
+        secret : str
+            the user secret generated on your ArcGIS dev portal page
+            
+        Returns
+        -------
+        str
+            a user token that can be used to access the ArcGIS world geocoding REST API endpoint
+        """
+
+        token_creator = """
+                        https://www.arcgis.com/sharing/oauth2/token?client_id={}&grant_type=client_credentials&client_secret={}&f=pjson
+                        """
+
+        res = get(token_creator.format(user, secret))
+
+        token_data = json.loads(res.content)
+
+        return token_data["access_token"]
+
+    def _make_request(self, payload):
+
+        token = self._get_token(self._env["ArcGIS_user"], self._env["ArcGIS_secret"])
+        params = {"token" : token}
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        api_url = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/geocodeAddresses"
+
+        r = post(url=api_url, params=params, data=payload, headers=headers)
+
+        return r
+
+    def _insert_user_location(self, ul):
+        """Inserts the user location info as a row into the PostgreSQL user locations table
+        
+        Parameters
+        ----------
+        ul : UserLocation
+        a UserLocation instance
+        """
+        querystring_template = """
+        INSERT INTO user_locations (location, geom, score, type, id) VALUES (%s, ST_SetSRID(ST_MakePoint(%s, %s),4326), %s, %s, %s)
+        """
+
+        query = self._cur.mogrify(querystring_template, (ul.loc_str, ul.geom[0], ul.geom[1], ul.score, ul.loc_type, ul.loc_id))
+        self._cur.execute(query)
+        self._conn.commit()
+        return 
+
+    def geocode(self):
         """Do the geocoding!
 
         Parameters
@@ -137,11 +194,33 @@ class Geocoder:
 
         Returns
         -------
-        """
-            
+        """      
+
+        locations = self._filter_duplicates()
+        location_objects = self._make_location_objects(locations)
+        payloads = self._make_addresses(location_objects, 200)
+
+        for payload in payloads:
+            res = self._make_request(payload)
+            if res.status_code != 200:
+                print(f"Something went wrong!")
+                break
+            else:
+                data = json.loads(res.content)
+                for geocoded_location in data["locations"]:
+                    u_loc = UserLocation()
+
+                    u_loc.loc_str = geocoded_location["attributes"]["LongLabel"]
+                    u_loc.geom = [geocoded_location["location"]["x"], geocoded_location["location"]["y"]]
+                    u_loc.score = geocoded_location["score"]
+                    u_loc.loc_type = geocoded_location["attributes"]["Type"]
+                    u_loc.loc_id = geocoded_location["attributes"]["ResultID"]
+
+                    self._insert_user_location(u_loc)
+                    print(f"Updated user location {u_loc.loc_str}")         
 
 class UserLocation:
-    def __init__(self, loc_str, geom, score, loc_type, loc_id):
+    def __init__(self, loc_str=None, geom=None, score=None, loc_type=None, loc_id=None):
         self._loc_str = loc_str
         self._geom = geom
         self._score = score
